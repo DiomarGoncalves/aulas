93c0a09f48a5ecac49112576bf55fe87a9cec1b8
Diomar Gonçalves <50972479+DiomarBR@users.noreply.github.com>
2023-07-22 09:10:50 -0300

    update curso novo

--- scripts.js
+++ scripts.js
@@ -1,13 +1,30 @@
 function abrirCurso(curso) {
-    // Oculta todas as seções de vídeos
-    var videoSections = document.getElementsByClassName("video-section");
-    for (var i = 0; i < videoSections.length; i++) {
-      videoSections[i].style.display = "none";
-    }
-  
-    // Exibe a seção de vídeos do curso selecionado
-    var cursoSection = document.getElementById(curso);
-    if (cursoSection) {
-      cursoSection.style.display = "block";
-    }
+  // Oculta todas as seções de vídeos e módulos
+  var videoSections = document.getElementsByClassName("video-section");
+  var moduloSections = document.getElementsByClassName("modulo-section");
+  for (var i = 0; i < videoSections.length; i++) {
+    videoSections[i].style.display = "none";
   }
+  for (var i = 0; i < moduloSections.length; i++) {
+    moduloSections[i].style.display = "none";
+  }
+
+  // Exibe a seção de vídeos ou módulos do curso selecionado
+  var cursoSection = document.getElementById(curso);
+  if (cursoSection) {
+    cursoSection.style.display = "block";
+  }
+}
+function abrirModulo(modulo) {
+  // Oculta todas as seções de módulos
+  var moduloSections = document.getElementsByClassName("modulo-section");
+  for (var i = 0; i < moduloSections.length; i++) {
+    moduloSections[i].style.display = "none";
+  }
+
+  // Exibe o módulo selecionado
+  var moduloSection = document.getElementById(modulo);
+  if (moduloSection) {
+    moduloSection.style.display = "block";
+  }
+}
